396aa7e82d8f7a8cebe8aa082988ea37c587e4ee
juanpablo2007k <juanpablo2007k@gmail.com>
2023-12-21 19:18:48 -0500

    new changed added

---
+++ src/main/resources/static/BogatyUsers.java
@@ -0,0 +1,11 @@
+package bogaty.online.bogaty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class BogatyUsers {
+public static  main(String[]args ){
+String Hello="hola";
+return Hello;
+}
+}
