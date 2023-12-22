22a3ceed188b3a88cb0c9de660bfe1e257e15b6d
juanpablo2007k <juanpablo2007k@gmail.com>
2023-12-21 19:11:41 -0500

    changed added

---
+++ src/main/java/bogaty/online/bogaty/BogatyApplication.java
@@ -0,0 +1,13 @@
+package bogaty.online.bogaty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BogatyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BogatyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bogaty/online/bogaty/BogatyApplicationTests.java
@@ -0,0 +1,13 @@
+package bogaty.online.bogaty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BogatyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
