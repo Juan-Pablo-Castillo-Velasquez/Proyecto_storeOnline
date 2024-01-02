0b9af8a7f6cd8ea35b37d68c07265a360afb0830
juanpablo2007k <juanpablo2007k@gmail.com>
2024-01-02 15:31:39 -0500

    new changed in this project

--- src/main/java/bogaty/online/bogaty/Controllers/indexControllers.java
+++ src/main/java/bogaty/online/bogaty/Controllers/indexControllers.java
@@ -1,42 +1,39 @@
 package bogaty.online.bogaty.Controllers;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-
-
-
-
-@Controller//espesificar controllador
+@Controller // espesificar controllador
 public class indexControllers {
-	@GetMapping(value = "/") //ruta vista
-public String index() {
-	return "index";
-}
-	@RequestMapping(value="/nueva_url_post", method=RequestMethod.GET) //htpp controller
-public String IndexRequestMapping() {
-		return "index";
-	}
-	@PostMapping(value="/")
-	public String indexPost(@RequestBody String index1, Model model) {
-	    try {
-	    	model.addAttribute("index1", index1);
-	    	System.out.println("Valor de index1 recibido: " + index1);
-	        return "index";
-	    } catch (Exception e) {
-	       
-	        e.printStackTrace();
-	        return "Error en el servidor";
-	    }
+	@GetMapping(value = "/")
+	public String indexPost(Model model) {
+		try {
+			String title1 = "add row notes";
+			model.addAttribute("model1", title1);
+			return "index";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "error";
+		}
 	}
 
+	@GetMapping(value="/about")
+	public String About() {
+		try {
+			return "about";
+			
+		}catch(Exception e){
+			e.printStackTrace();
+			return "error";
+		}
+	}
+	/**
+	 * @PostMapping(value = "/xd") public String handlePost(String data, Model
+	 *                    model) { model.addAttribute("info", data);
+	 *                    model.addAttribute("condicion", true);
+	 *                    System.out.println("Data received in POST: " + data);
+	 *                    return "redirect:/xd/result"; }
+	 */
+
 }
-
-
-
